9fda9ce75262825ae2085cd6a453b685191ba991
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-06-24 19:33:52 +0300

    Refactor OtherSheetsProcess to improve sheet processing logic and enhance header validation

--- Workflows/Excel/OtherSheetsProcess.cs
+++ Workflows/Excel/OtherSheetsProcess.cs
@@ -1,6 +1,9 @@
 using ProcessDocumentFunction.Models.Excel;
 using DocumentFormat.OpenXml.Packaging;
 using Microsoft.Extensions.Logging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ProcessDocumentFunction.Models.Constants.Excel;
+using ProcessDocumentFunction.Services.Excel;
 
 namespace ProcessDocumentFunction.Workflows.Excel;
 
@@ -9,10 +12,36 @@ public class OtherSheetsProcess(ILogger<OtherSheetsProcess> logger)
   private readonly ILogger<OtherSheetsProcess> _logger = logger;
 
   public void Execute(
-    WorkbookPart workbookPart, 
-    IEnumerable<ReportLogOutData?> sheetsInfoToProcess, 
+    WorkbookPart workbookPart,
+    IEnumerable<ReportLogOutData?> sheetsInfoToProcess,
     bool isFormattingOnly)
   {
-    
+    foreach (var sheetInfo in sheetsInfoToProcess)
+    {
+      if (string.IsNullOrEmpty(sheetInfo?.SheetName)) continue;
+      _logger.LogInformation("Start process sheet: {SheetName}", sheetInfo.SheetName);
+
+      var sheets = workbookPart.Workbook.Sheets?.OfType<Sheet>() ?? [];
+      var sheet = sheets.FirstOrDefault(s => s.Name?.Value == sheetInfo.SheetName);
+      if (sheet == null) return;
+
+      var headers = new string[4] { OtherDataConfig.TitleColumnName,
+        OtherDataConfig.DateColumnName,
+        OtherDataConfig.UserNameColumnName,
+        OtherDataConfig.TimeSpentColumnName };
+      sheetInfo.HeaderIndex = ExcelReader.SearchForHeaders(headers, sheetInfo, false, false, out List<string> missingHeaders);
+      if (missingHeaders.Count != 0)
+        throw new Exception($"There are missing required headers on the sheet {sheetInfo.SheetName} : {string.Join(", ", missingHeaders)}");
+
+      ExcelUpdater.SetHeadersFilter(sheetInfo, workbookPart);
+      ExcelUpdater.SetColumnsWidth(sheetInfo, workbookPart);
+      if (isFormattingOnly)
+      {
+        _logger.LogInformation($"FormatingOnly. End process sheet: {sheetInfo.SheetName}");
+        return;
+      }
+
+      _logger.LogInformation($"End process sheet: {sheetInfo.SheetName}");
+    }
   }
 }
